87a0bcc8fa31b96be0708878a108feb7efbae5b2
Natanael <namuji38@gmail.com>
2026-06-21 12:41:00 -0400

    fix: win hotreload pd: david weco

--- frontend/src/app/login/page.jsx
+++ frontend/src/app/login/page.jsx
@@ -3,7 +3,11 @@
 export default function Home() {
   return (
   <div>
+<<<<<<< HEAD
     jorge weno pal pico de los primos
+=======
+    aqui
+>>>>>>> 7a87799 (fix: win hotreload)
   </div>
   );
 }
