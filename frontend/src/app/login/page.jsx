a10a8024cd9eaacecda78f4350878ee5560dc574
Natanael <namuji38@gmail.com>
2026-06-21 12:48:16 -0400

    prueba

--- frontend/src/app/login/page.jsx
+++ frontend/src/app/login/page.jsx
@@ -3,11 +3,7 @@
 export default function Home() {
   return (
   <div>
-<<<<<<< HEAD
-    jorge weno pal pico de los primos
-=======
     aqui
->>>>>>> 7a87799 (fix: win hotreload)
   </div>
   );
 }
